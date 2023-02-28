9b938f6a5b3d84985199247b4c5f4e3b2b395b6e
Exequiel Flores <efloresa93@gmail.com>
2023-02-27 23:47:30 -0300

    Primer Commit

---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,15 @@
+
+console.log("Hola mundo !!");
+console.warn("Hola mundo !!");
+console.error("Hola mundo !!");
+
+let a = 2;
+let b = 3;
+let c = 7;
+let d = 1;
+
+console.log('%c MIs variables', 'color: blue; font-weight: bold;')
+console.log({  a });
+
+console.table({ a,b,c,d });
+
